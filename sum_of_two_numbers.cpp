3a0e7afce86fff137cdcfd9bcfa01d932568605a
Amit Kumar <amitsharma983568@gmail.com>
2026-05-07 23:36:12 +0530

    Add sum of two numbers program

---
+++ sum_of_two_numbers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"enter 1st number:";
+    int x;
+    cin>>x;
+    cout<<"enter 2nd number:";
+    int y;
+    cin>>y;
+    cout<<"Sum is: "<<x+y <<endl;
+    return 0;
+}
